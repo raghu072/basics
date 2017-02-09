7df3834c59e3827cfb734e2b63e385cf04c4e949
Raghavendra Purohit <raghavendra.purohit@TheoremIndia.net>
2017-02-09 17:03:51 +0530

    view changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,11 +6,7 @@
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 </head>
 <body>
-<div class="container">
-	<div class="row">
-		Raghavendra
-	</div>
-</div>
+
 	<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </body>
